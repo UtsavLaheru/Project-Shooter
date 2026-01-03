86d63f758f89a9c6d6d97cd18bf7ea8f7b2d6dc8
turtletennis <stephenjenkins42@gmail.com>
2026-01-03 18:45:24 +0000

    Add sound effect for pickup and use it when a pickup is collected

--- shooter/components/pickup.gd
+++ shooter/components/pickup.gd
@@ -5,6 +5,8 @@ func _on_body_entered(body: Node2D) -> void:
 	if(!picked_up && body is Player):
 		picked_up = true
 		apply_pickup(body)
+		var audio_manager:AudioManager = get_tree().get_first_node_in_group("audio_manager")
+		audio_manager.playPickupStream(global_position)
 		queue_free()
 
 func apply_pickup(player:Player):
